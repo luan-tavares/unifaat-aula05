1403dafaa1e7da08e2bf33a350e9435302073c95
Luan Lourenço <luan.tavares.lourenco@gmail.com>
2025-03-24 13:12:36 -0300

    Third Commit

--- src/promise-block.js
+++ src/promise-block.js
@@ -1,17 +1,19 @@
-// Exemplo 1: Promise
+    // Exemplo 1: Promise
 
-function tarefaDemorada() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('Tarefa concluída!');
-        }, 2000); // Simula uma tarefa demorada de 2 segundos
+    function tarefaDemorada() {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve('Tarefa concluída!');
+            }, 2000); // Simula uma tarefa demorada de 2 segundos
+        });
+    }
+
+
+
+    console.log('Iniciando a tarefa...');
+
+    tarefaDemorada().then(result => {
+        console.log(result);  // Isso é executado após 2 segundos
     });
-}
 
-console.log('Iniciando a tarefa...');
-
-tarefaDemorada().then(result => {
-    console.log(result);  // Isso é executado após 2 segundos
-});
-
-console.log('Código continua executando enquanto aguarda a tarefa...');
+    console.log('Código continua executando enquanto aguarda a tarefa...');
